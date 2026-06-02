c6e3fe832c8d1af802896da540a3e3b000f33ebb
M1-19 <mertcaglarim@gmail.com>
2026-06-02 12:35:17 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -60,7 +60,7 @@ public:
 
     // TODO 3: Override area().  Area of a circle = PI * r * r.
     //         Use override.
-    double area() const override { return 3.14 *radius * radius; }
+    double area() const override { return 3.14159265358979323846 *radius * radius; }
 };
 
 // --- Derived class: Rectangle -----------------------------------
